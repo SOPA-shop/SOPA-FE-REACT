83c0ca3db99b035098f61127b910ac79b2fac1fc
Charlie Nguyen <vawntanng@gmail.com>
2023-11-07 23:22:54 +0700

    [REFACTOR] add plugin-image in vite

--- vite.config.js
+++ vite.config.js
@@ -1,6 +1,7 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
+import image from '@rollup/plugin-image'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), image()],
 })
