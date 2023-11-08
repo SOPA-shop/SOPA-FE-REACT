4925a9e42ac217299837782a39d8e05d267d06cb
Charlie Nguyen <vawntanng@gmail.com>
2023-11-08 22:10:11 +0700

    [REFACTOR] refactor import img

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,5 +1,5 @@
 import './Footer.scss';
-import logo from '../../assets/image/logo.svg';
+import logo from '../../assets/image/Logo.svg';
 import logoInstagram from '../../assets/image/instagram.svg';
 import logoFacebook from '../../assets/image/facebook.svg';
 import logoTwitter from '../../assets/image/twitter.svg';
@@ -13,145 +13,162 @@ export default function Footer() {
         <Row className="footer__container">
           <Col xs={16} className="footer__logo">
             <div className="footer__logo__wraper">
-              <img className='footer__logo__wraper__img'
-                src={logo} alt="iconLogo" />
+              <img
+                className="footer__logo__wraper__img"
+                src={logo}
+                alt="iconLogo"
+              />
             </div>
-            <p className='footer__logo__title'>
-              Stay informed about Sopa with our
-              latest releases and founder news.
+            <p className="footer__logo__title">
+              Stay informed about Sopa with our latest releases and founder
+              news.
             </p>
-            <button className='footer__logo__btn'>
-              Enter email here for updates</button>
+            <button className="footer__logo__btn">
+              Enter email here for updates
+            </button>
           </Col>
           <Col xs={2} className="footer__col">
             <div className="footer__col__items">
-              <h3 className='footer__col__items__title'>Product</h3>
-              <ul className='footer__col__items__list'>
-                <li className='footer__col__items__list__item'>
-                  <Link
-                    className='footer__col__items__list__item__link'
-                    to="#"
-                  >
+              <h3 className="footer__col__items__title">Product</h3>
+              <ul className="footer__col__items__list">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     Model 000
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     Model 001
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     Laces
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     Masks
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     No-show Socks
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     Crew Socks
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link
-                    className='footer__col__items__list__item__link'
-                    to="#">Gift Cards</Link>
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    Gift Cards
+                  </Link>
                 </li>
               </ul>
             </div>
           </Col>
           <Col xs={2} className="footer__col">
             <div className="footer__col__items">
-              <h3 className='footer__col__items__title'>Support</h3>
-              <ul className='footer__col__items__list'>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+              <h3 className="footer__col__items__title">Support</h3>
+              <ul className="footer__col__items__list">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     Help Center
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     FAQs
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     Order
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">Order Status</Link>
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    Order Status
+                  </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
-                    Returns & Exchanges</Link>
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    Returns & Exchanges
+                  </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">Contact Us</Link>
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    Contact Us
+                  </Link>
                 </li>
               </ul>
             </div>
           </Col>
           <Col xs={2} className="footer__col">
             <div className="footer__col__items">
-              <h3 className='footer__col__items__title'>Everything Else</h3>
-              <ul className='footer__col__items__list'>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">Community</Link>
+              <h3 className="footer__col__items__title">Everything Else</h3>
+              <ul className="footer__col__items__list">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    Community
+                  </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">Why Sopa</Link>
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    Why Sopa
+                  </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">About</Link>
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    About
+                  </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
                     Discount Program
                   </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">Sopa Blog</Link>
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    Sopa Blog
+                  </Link>
                 </li>
-                <li className='footer__col__items__list__item'>
-                  <Link className='footer__col__items__list__item__link' to="#">
-                    Sopa Ambassadors</Link>
+                <li className="footer__col__items__list__item">
+                  <Link className="footer__col__items__list__item__link" to="#">
+                    Sopa Ambassadors
+                  </Link>
                 </li>
               </ul>
             </div>
           </Col>
           <Col xs={2} className="footer__contact">
-            <ul className='footer__contact__list'>
+            <ul className="footer__contact__list">
               <li className="footer__contact__list__li">
                 <Link className="footer__contact__list__li__link" to="#">
                   <img
-                    className='footer__contact__list__li__link__img'
-                    src={logoTwitter} alt="twitter" />
+                    className="footer__contact__list__li__link__img"
+                    src={logoTwitter}
+                    alt="twitter"
+                  />
                   <span>Twitter</span>
                 </Link>
               </li>
-              <li className='footer__contact__list__li'>
+              <li className="footer__contact__list__li">
                 <Link className="footer__contact__list__li__link" to="#">
                   <img
-                    className='footer__contact__list__li__link__img'
+                    className="footer__contact__list__li__link__img"
                     src={logoInstagram}
                     alt="instagram"
                   />
                   <span>Instagram</span>
                 </Link>
               </li>
-              <li className='footer__contact__list__li'>
+              <li className="footer__contact__list__li">
                 <Link className="footer__contact__list__li__link" to="#">
                   <img
-                    className='footer__contact__list__li__link__img'
+                    className="footer__contact__list__li__link__img"
                     src={logoFacebook}
                     alt="facebook"
                   />
@@ -163,5 +180,5 @@ export default function Footer() {
         </Row>
       </Grid>
     </div>
-  )
+  );
 }
