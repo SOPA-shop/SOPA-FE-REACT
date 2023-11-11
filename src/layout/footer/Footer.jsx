781753848d4153263e81c0e375e19a91196364dc
Henry-Noise <116413215+Henry-Noise@users.noreply.github.com>
2023-11-11 10:44:24 +0700

    [ADD] footer ui (#7)
    * [ADD] footer ui
    
    * [REFACTOR] refactor config file
    
    * [REFACTOR] refactor config file
    
    * [REFACTOR] refactor import img
    
    * [REFACTOR] refactor layout footer
    
    * [REFACTOR] refactor color footer
    
    * [REFACTOR] rewritten as component
    
    * [REFACTOR] change scss
    
    * [REFACTOR] change footer contact to component
    
    ---------
    
    Co-authored-by: Charlie Nguyen <vawntanng@gmail.com>

--- src/layout/footer/Footer.jsx
+++ src/layout/footer/Footer.jsx
@@ -1,8 +1,127 @@
+import './Footer.scss';
+import logo from '../../assets/image/Logo.svg';
+import logoInstagram from '../../assets/image/instagram.svg';
+import logoFacebook from '../../assets/image/facebook.svg';
+import logoTwitter from '../../assets/image/twitter.svg';
+import { Col, Grid, Row } from 'rsuite';
+import { Link } from 'react-router-dom';
+
 export default function Footer() {
   return (
-    <div>
-      <h1>Footer</h1>
-      <div>FOOTER DEVELOPO</div>
+    <div className="footer">
+      <Grid fluid>
+        <Row className="footer__container">
+          <Col className="footer__wraper">
+            <div className="footer__logo">
+              <div className="footer__logo__wraper">
+                <img
+                  className="footer__logo__wraper__img"
+                  src={logo}
+                  alt="iconLogo"
+                />
+              </div>
+              <p className="footer__logo__title">
+                Stay informed about Sopa with our latest releases and founder
+                news.
+              </p>
+              <button className="footer__logo__btn">
+                Enter email here for updates
+              </button>
+            </div>
+          </Col>
+          <div className="footer__col__wrap">
+            {dataFooter.map((item, index) => {
+              return (
+                <Col className="footer__col" key={index}>
+                  <div className="footer__col__items">
+                    <h3 className="footer__col__items__title">{item.title}</h3>
+                    <ul className="footer__col__items__list">
+                      {item.list.map((item, indexItem) => {
+                        return (
+                          <li className="footer__col__items__list__item" key={indexItem}>
+                            <Link className="footer__col__items__list__item__link" to="#">
+                              {item}
+                            </Link>
+                          </li>
+                        );
+                      })}
+                    </ul>
+                  </div>
+                </Col>
+              )
+            })}
+            <Col className="footer__contact">
+              <ul className="footer__contact__list">
+                {dataContactFooter.map((item, index) => {
+                  return (
+                    <li className="footer__contact__list__li" key={index}>
+                      <Link className="footer__contact__list__li__link" to="#">
+                        <img
+                          className="footer__contact__list__li__link__img"
+                          src={item.image}
+                          alt="twitter"
+                        />
+                        <span>{item.title}</span>
+                      </Link>
+                    </li>
+                  );
+                })}
+              </ul>
+            </Col>
+          </div>
+        </Row>
+      </Grid>
     </div>
-  )
+  );
 }
+const dataFooter = [
+  {
+    title: 'Product',
+    list: [
+      'Model 000',
+      'Model 001',
+      'Laces',
+      'Masks',
+      'No-show Socks',
+      'Crew Socks',
+      'Gift Cards'
+    ]
+  },
+  {
+    title: 'Support',
+    list: [
+      'Help Center',
+      'FAQs',
+      'Order',
+      'Order Status',
+      'Returns & Exchanges',
+      'Contact Us'
+    ]
+  },
+  {
+    title: 'Everything Else',
+    list: [
+      'Community',
+      'Why Sopa',
+      'About',
+      'Discount Program',
+      'Sopa Blog',
+      'Sopa Ambassador'
+    ]
+  }
+]
+
+const dataContactFooter = [
+  {
+    image: logoTwitter,
+    title: 'Twitter',
+  },
+  {
+    image: logoInstagram,
+    title: 'Instagram',
+  },
+  {
+    image: logoFacebook,
+    title: 'Facebook',
+  },
+]
--- vite.config.js
+++ vite.config.js
@@ -1,18 +1,6 @@
-import * as path from 'path'
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
-const __dirname = path.resolve(path.dirname(''))
-
 export default defineConfig({
   plugins: [react()],
-  resolve: {
-    alias: {
-      '@': path.resolve(__dirname, './src/'),
-      assets: `${path.resolve(__dirname, './src/assets/')}`,
-      public: `${path.resolve(__dirname, './public/')}`,
-      pages: path.resolve(__dirname, './src/pages'),
-      types: `${path.resolve(__dirname, './src/@types')}`,
-    },
-  },
 })
